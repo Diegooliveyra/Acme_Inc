9e00b71ab72555ee5986e986f883cdb9b71c28b5
Diego Oliveira <diegooliveirabryto@gmail.com>
2023-10-30 09:25:06 -0300

    chore: add styled-component

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,34 @@
+'use client';
+
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family:  'Open Sans', sans-serif;
+  }
+
+  html {
+    font-size: 62.5%;
+    scroll-behavior: smooth;
+  }
+
+  hr {
+    opacity: 0.3;
+    cursor: pointer;
+  }
+
+  html, body, #__next {
+    height: 100%;
+  }
+
+
+  body {
+    background-color: #000;
+    color: #FFF
+  }
+`;
+
+export default GlobalStyles;
