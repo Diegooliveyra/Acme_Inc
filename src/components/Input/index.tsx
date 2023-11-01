4dd5ece249a787cb1c605faf3fca93847e1afe42
Diego Oliveira <diegooliveirabryto@gmail.com>
2023-11-01 10:59:24 -0300

    feat: input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,4 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { InputHTMLAttributes, useEffect, useState } from 'react';
-
-import { ReactSVG } from 'react-svg';
 
 import * as S from './styles';
 
@@ -9,35 +6,27 @@ export type isDefaultType = 'default' | 'small';
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   disabled?: boolean;
-  iconLeft?: any;
+  label: string;
   placeholder?: string;
   required?: boolean;
-  type?: 'number' | 'text' | 'password' | 'date' | 'email' | 'month';
-  onClickInIconRight?: boolean;
+  type?: 'number' | 'text' | 'password' | 'date' | 'email' | 'tel';
   value?: string;
   initialValue?: any;
   handleOnChange?: (value: any) => any;
   handleOnFocus?: () => any;
   handleOnBlur?: () => any;
-  iconRight?: any;
-  onClickIconRight?: () => void;
-  onClickIconLeft?: () => void;
 }
 
 const Input = ({
   placeholder = '',
   type = 'text',
-  iconLeft = '',
-
   value = '',
+  label,
   handleOnChange,
   handleOnBlur,
   handleOnFocus,
   required,
   className,
-  iconRight = '',
-  onClickIconLeft,
-  onClickIconRight,
   disabled = false,
   initialValue,
 
@@ -47,85 +36,35 @@ const Input = ({
     initialValue !== '' ? initialValue : value
   );
 
-  const [isShowPassword, setIsShowPassword] = useState(false);
-  const [typeState, setType] = useState(type);
-  const [isFocus, setIsFocus] = useState(false);
-  const [isFill, setIsFill] = useState(false);
-
   const changeInputValue = (event: React.FormEvent<HTMLInputElement>) => {
     let value = event.currentTarget.value;
     setValue(value);
     !!handleOnChange && handleOnChange!(value);
   };
 
-  function onFocus() {
-    setIsFocus(true);
-    !!handleOnFocus && handleOnFocus();
-  }
-
-  function onBlur() {
-    setIsFocus(false);
-    setIsFill(!!valueState);
-    !!handleOnBlur && handleOnBlur();
-  }
-
-  function onClickVisibilit() {
-    const val = !isShowPassword;
-    setIsShowPassword(val);
-    if (val) return setType(type);
-    return setType('password');
-  }
-
-  function clickedIconRight() {
-    !!onClickIconRight && onClickIconRight();
-  }
-
   useEffect(() => {
     if (initialValue !== undefined && initialValue !== '') {
       setValue(initialValue);
-
-      onBlur();
     } else {
       setValue(value);
     }
   }, [initialValue, value]);
 
   return (
-    <>
-      <S.Wrapper
-        isFocus={isFocus}
-        isFill={true}
-        isIconLeft={!!iconLeft}
-        className={className}
-        disabled={disabled}
-      >
-        <S.WrapperLeft>
-          {!!iconLeft && (
-            <S.IconLeft onClick={onClickIconLeft}>
-              <ReactSVG src={iconLeft ? iconLeft : ''} wrapper="span" />
-            </S.IconLeft>
-          )}
-
-          <S.Input
-            value={valueState}
-            placeholder={placeholder}
-            onChange={changeInputValue}
-            onFocus={onFocus}
-            onBlur={onBlur}
-            onWheel={(event) => event.currentTarget.blur()}
-            type={typeState}
-            disabled={disabled}
-            {...rest}
-          />
-        </S.WrapperLeft>
-
-        {!!iconRight && (
-          <S.ButtonIconRight type="button" onClick={clickedIconRight}>
-            <ReactSVG src={iconRight} wrapper="span" />
-          </S.ButtonIconRight>
-        )}
+    <S.Container>
+      <S.Label htmlFor="">{label}</S.Label>
+      <S.Wrapper className={className} disabled={disabled}>
+        <S.Input
+          value={valueState}
+          placeholder={placeholder}
+          onChange={changeInputValue}
+          onWheel={(event) => event.currentTarget.blur()}
+          type={type}
+          disabled={disabled}
+          {...rest}
+        />
       </S.Wrapper>
-    </>
+    </S.Container>
   );
 };
 
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -5,33 +5,23 @@ import styled, { css, DefaultTheme } from 'styled-components';
 import theme from '../../styles/theme';
 
 type WrapperProps = {
-  isFill: boolean;
-  isFocus: boolean;
-  isIconLeft: boolean;
-
   disabled?: boolean;
 };
 
-// COMPONENTES
+export const Container = styled.div`
+  ${() => css`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  `}
+`;
+
 export const Label = styled.label`
   ${() => css`
-    position: absolute;
-    left: 8px;
-    top: calc(50% - 10px);
-    color: #aaa;
-
-    font-size: 1.6rem;
-    color: ${theme.colors.white};
-    font-weight: 400;
-    padding: 0 0.8rem;
-
-    transition: all 0.1s ease;
-
-    pointer-events: none;
-
-    span {
-      color: red;
-    }
+    font-size: ${theme.font.sizes.button};
+    color: ${theme.colors.gray.medium};
+    line-height: ${theme.spacings.small};
+    font-weight: ${theme.font.normal};
   `}
 `;
 
@@ -39,16 +29,11 @@ export const Input = styled.input`
   position: relative;
   border: none;
   background: transparent;
-  color: #fff;
-  transition: all 0.2s ease;
-
+  color: ${theme.colors.white};
   height: 100%;
   width: 100%;
-
   outline: none;
-
-  font-size: 1.6rem;
-
+  font-size: ${theme.font.sizes.body};
   ::-webkit-outer-spin-button,
   ::-webkit-inner-spin-button {
     -webkit-appearance: none;
@@ -56,138 +41,21 @@ export const Input = styled.input`
   }
 `;
 
-export const IconLeft = styled.div`
-  ${() => css`
-    margin-right: 0.8rem;
-    margin-top: 6px;
-
-    svg path {
-      fill: ${theme.colors.purple.light};
-    }
-  `}
-`;
-
-export const ButtonPassword = styled.div`
-  ${() => css`
-    border: none;
-    background: transparent;
-    margin: 0;
-    padding: 0;
-    outline: none;
-    margin-top: 2px;
-
-    margin-left: 0.8rem;
-
-    svg path {
-      fill: ${theme.colors.purple.light};
-    }
-  `}
-`;
-
-export const ButtonIconRight = styled.button`
-  ${() => css`
-    border: none;
-    background: transparent;
-
-    cursor: pointer;
-
-    margin: 0;
-    padding: 0;
-    outline: none;
-    margin-top: 2px;
-
-    margin-left: 0.8rem;
-
-    svg path {
-      fill: ${theme.colors.purple.light};
-    }
-  `}
-`;
-
-export const WrapperLeft = styled.div`
-  width: 100%;
-  height: 100%;
-
-  display: flex;
-  align-items: center;
-`;
-
 export const Wrapper = styled.div<WrapperProps>`
-  ${({ isFill, isFocus, isIconLeft, disabled }) => css`
+  ${({ disabled }) => css`
     width: 100%;
     position: relative;
-   
     height: 5.6rem;
-
     padding: 0 1.6rem;
     border: 1px solid ${theme.colors.black.light};
     border-radius: 0.4rem;
-
     display: flex;
-    flex-direction: row !important;
-    align-items: center;
-
-    background: transparent
+    flex-direction: column;
     opacity: ${disabled ? '0.6' : '1'};
-    .loading {
-      width: 30px;
-    }
+    border: 1px solid ${theme.colors.purple.light};
 
-    ${
-      isIconLeft &&
-      css`
-        ${Label} {
-          left: 40px;
-        }
-      `
-    }
-
-    ${
-      isFill &&
-      css`
-        border: 1px solid ${theme.colors.purple.light};
-
-        ${Input} {
-          border: none;
-        }
-
-        ${Label} {
-          top: -10px;
-          left: 8px;
-          font-size: 1.4rem;
-
-          z-index: 1;
-
-          padding: 0 0.4rem;
-
-          background: ${theme.colors.black.dark};
-
-          color: ${theme.colors.purple.light};
-        }
-      `
-    }
-
-    ${
-      isFocus &&
-      css`
-        border: 2px solid ${theme.colors.purple.light} !important;
-
-        ${Input} {
-          border: none;
-        }
-
-        ${Label} {
-          top: -10px;
-          left: 8px;
-          font-size: 1.4rem;
-
-          z-index: 1;
-
-          padding: 0 0.4rem;
-
-          color: ${theme.colors.purple.light} !important;
-        }
-      `
+    ${Input} {
+      border: none;
     }
   `}
 `;
