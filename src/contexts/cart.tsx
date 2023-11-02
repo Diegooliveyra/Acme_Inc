7177cf1f29668f19934d834a8fd043fe9acb51cf
Diego Oliveira <diegooliveirabryto@gmail.com>
2023-11-02 10:54:48 -0300

    feat: cart context

---
+++ src/contexts/cart.tsx
@@ -0,0 +1,116 @@
+'use client'
+
+import { IProductoDTO } from '@/types/product'
+import { ReactNode, createContext, use, useEffect, useMemo, useState } from 'react'
+
+export interface CartProduct extends IProductoDTO {
+  quantity: number
+}
+
+interface ICartContext {
+  products: CartProduct[]
+  total: number
+  addProductToCart: (product: CartProduct) => void
+  decreaseProductQuantity: (productId: number) => void
+  increaseProductQuantity: (productId: number) => void
+  removeProductFromCart: (productId: number) => void
+}
+
+export const CartContext = createContext<ICartContext>({
+  products: [],
+  total: 0,
+  addProductToCart: () => {},
+  decreaseProductQuantity: () => {},
+  increaseProductQuantity: () => {},
+  removeProductFromCart: () => {},
+})
+
+const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([])
+
+  useEffect(() => {
+    localStorage.setItem('@fsw-store/cart-products', JSON.stringify(products))
+  }, [products])
+
+  const total = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + product.value * product.quantity
+    }, 0)
+  }, [products])
+
+  const addProductToCart = (product: CartProduct) => {
+    const productIsAlreadyOnCart = products.some((cartProduct) => cartProduct.id === product.id)
+
+    if (productIsAlreadyOnCart) {
+      setProducts((prev) =>
+        prev.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + product.quantity,
+            }
+          }
+
+          return cartProduct
+        }),
+      )
+
+      return
+    }
+
+    setProducts((prev) => [...prev, product])
+  }
+
+  const decreaseProductQuantity = (productId: number) => {
+    setProducts((prev) =>
+      prev
+        .map((cartProduct) => {
+          if (cartProduct.id === productId) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity - 1,
+            }
+          }
+
+          return cartProduct
+        })
+        .filter((cartProduct) => cartProduct.quantity > 0),
+    )
+  }
+
+  const increaseProductQuantity = (productId: number) => {
+    setProducts((prev) =>
+      prev.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + 1,
+          }
+        }
+
+        return cartProduct
+      }),
+    )
+  }
+
+  const removeProductFromCart = (productId: number) => {
+    setProducts((prev) => prev.filter((cartProduct) => cartProduct.id !== productId))
+  }
+
+  return (
+    <CartContext.Provider
+      value={{
+        products,
+        addProductToCart,
+        decreaseProductQuantity,
+        increaseProductQuantity,
+        removeProductFromCart,
+        total,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export default CartProvider
